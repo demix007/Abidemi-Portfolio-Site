db0c657e6938ba56271ad8c0e59404c48ce708c7
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2022-09-16 11:23:35 +0100

    Add Form validation

---
+++ js/formValidation.js
@@ -0,0 +1,25 @@
+const validateEmail = {
+    form: document.getElementById('contactForm'),
+    email: document.getElementById('email'),
+    errorMessage: null,
+};
+
+const validationLogic = {
+    start() {
+        validateEmail.form.addEventListener('submit', (e) => {
+            if (validateEmail.email.value.toLowerCase() != validateEmail.email.value) {
+                e.preventDefault();
+                validationLogic.error();
+            }
+        })
+    },
+
+    error() {
+        validateEmail.form
+            .appendChild(document.createElement('P'))
+            .setAttribute('id', 'error');
+        validateEmail.errorMessage = document.getElementById('error');
+        validateEmail.errorMessage.innerText = 'Please, use only lowercase letters for your email!';
+    },
+};
+validationLogic.start();
